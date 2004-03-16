f82b1f5400525f4c9dc9399f224c92597403ec6f
Hans Ulrich Niedermann <hun@n-dimensional.de>
2004-03-16 01:42:52 +0000

    add tla revision to package info
    add tla revision to package info
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/ndim-utils--main--0.1--patch-14

--- misc/pkg-info.c
+++ misc/pkg-info.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "config.h"
+#include "pkg-info.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,12 +10,15 @@ int main(int argc, char *argv[])
 	 "Short name: %s\n"
 	 "PVersion:   %s\n"
 	 "Version:    %s\n"
-	 "Bugreports: %s\n",
+	 "TLA Rev:    %s\n"
+	 "Bugreports: %s\n"
+	 ,
 	 PACKAGE,
 	 PACKAGE_NAME,
 	 PACKAGE_TARNAME,
 	 PACKAGE_VERSION,
 	 VERSION,
+	 TLA_REVISION,
 	 PACKAGE_BUGREPORT
 	 );
   return 0;
