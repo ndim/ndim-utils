c74d19eae7ad4d1f5cf537afef3a1bcf8aeebd4f
Hans Ulrich Niedermann <hun@n-dimensional.de>
2004-05-03 13:44:30 +0000

    added one.c
    added one.c
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/ndim-utils--main--0.1--patch-45

---
+++ misc/one.c
@@ -0,0 +1,35 @@
+/**
+ * writes file "one.tmp" into current directory until 
+ * error occurs (disk full, quota exceeded)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE (1 << 20)
+
+int main(int argc, char *argv[]) {
+  FILE *file;
+  char *buf;
+  int i;
+  size_t count;
+  file = fopen("one.tmp","w");
+  buf = malloc(BUFSIZE);
+  if (!file || !buf) {
+    printf("Error.");
+    return(1);
+  }
+  for (i=0; i<BUFSIZE; i++) {
+    buf[i]=i;
+  }
+  i = 0;
+  do {
+    count = fwrite(buf,sizeof(char),BUFSIZE,file);
+    i++;
+  } while (count == BUFSIZE);
+  printf("Finished. (count,i) = (%d,%d)\n",count,i);
+  fclose(file);
+  free(buf);
+}
+
+/* arch-tag: 4c015a35-7193-4bbc-8c53-b86254656882 */
