8c1f48a88797ca47e6e31e4c08cbdfc57caa7c37
Hans Ulrich Niedermann <hun@n-dimensional.de>
2004-06-09 02:46:44 +0000

    use python distutils for sysadmin libs
    use python distutils for sysadmin libs
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/ndim-utils--main--0.1--patch-52

---
+++ admin-py/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name="PysAdm",
+      version="0.0.0",
+      description="Python System Administration Utilities",
+      author="Hans Ulrich Niedermann",
+      author_email="hun@n-dimensional.de",
+      url="http://n-dimensional.de/",
+      py_modules=["whois"])
+
+# arch-tag: 4344ed7a-f67f-48cc-8db7-384d30c8e230
