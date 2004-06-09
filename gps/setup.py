71cf703ffb420ef31df9c04cafe6e2244ab19e41
Hans Ulrich Niedermann <hun@n-dimensional.de>
2004-06-09 02:49:29 +0000

    use python distutils for gps libs
    use python distutils for gps libs
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/ndim-utils--main--0.1--patch-54

---
+++ gps/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name="GpsTools",
+      version="0.0.0",
+      description="Python GPS Tools",
+      author="Hans Ulrich Niedermann",
+      author_email="hun@n-dimensional.de",
+      url="http://n-dimensional.de/",
+      py_modules=["geocoord"])
+
+# arch-tag: d31c4bba-e916-4547-b337-ca7f8264d33b
