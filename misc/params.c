61b7e96089b79df68c984f735fb221ca7325c0e2
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-12-01 05:17:28 +0000

    let params print both environment and parameters
    let params print both environment and parameters
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/ndim-utils--main--0.1--patch-92

--- misc/params.c
+++ misc/params.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *env[])
 {
 	int i;
+	
+	printf("#nr environment variable\n");
+	for (i=0; env[i] != NULL; i++) 
+		printf("%3d %s\n", i, env[i]);
+	
 	printf("#nr parameter\n");
 	for (i=0; i<argc; i++) 
 		printf("%3d %s\n", i, argv[i]);
+	
 	return 0;
 }
