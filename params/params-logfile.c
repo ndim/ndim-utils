6e7f069a65be20b05fd6283b87d7f0efc77bd8d8
Hans Ulrich Niedermann <hun@n-dimensional.de>
2014-12-20 21:26:00 +0100

    params: reindent code

--- params/params-logfile.c
+++ params/params-logfile.c
@@ -11,14 +11,23 @@ const char *logfile_name = LOGFILE_NAME;
 
 int main(int argc, char *argv[], char *env[])
 {
-	FILE *logfile = fopen(logfile_name, "a");
+    FILE *logfile = fopen(logfile_name, "a");
 
-	if (!logfile) {
-		const char *errstr = strerror(errno);
-		fprintf(stderr, "FATAL: %s: %s\n", errstr, logfile_name);
-		return EXIT_FAILURE;
-	}
+    if (!logfile) {
+        const char *errstr = strerror(errno);
+        fprintf(stderr, "FATAL: %s: %s\n", errstr, logfile_name);
+        return EXIT_FAILURE;
+    }
 
-	report_params(logfile, argc, argv, env);
-	return EXIT_SUCCESS;
+    report_params(logfile, argc, argv, env);
+    return EXIT_SUCCESS;
 }
+
+
+/*
+ * Local Variables:
+ * indent-tabs-mode: nil
+ * c-basic-offset: 4
+ * tab-width: 8
+ * End:
+ */
--- params/params-stdout.c
+++ params/params-stdout.c
@@ -6,6 +6,15 @@
 
 int main(int argc, char *argv[], char *env[])
 {
-	report_params(stdout, argc, argv, env);
-	return EXIT_SUCCESS;
+    report_params(stdout, argc, argv, env);
+    return EXIT_SUCCESS;
 }
+
+
+/*
+ * Local Variables:
+ * indent-tabs-mode: nil
+ * c-basic-offset: 4
+ * tab-width: 8
+ * End:
+ */
