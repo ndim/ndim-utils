dc42b5fce1df49442d02270a84321deeeba01fc7
Hans Ulrich Niedermann <hun@n-dimensional.de>
2014-12-20 14:52:10 +0100

    params: Restructure the code

--- params/report-params.c
+++ params/report-params.c
@@ -3,11 +3,16 @@
 
 void report_params(FILE *out, int argc, char *argv[], char *env[])
 {
-	fprintf(out, "#nr environment variable\n");
-	for (int i=0; env[i] != NULL; i++)
-		fprintf(out, "%3d %s\n", i, env[i]);
+	do {
+		fprintf(out, "#nr environment variable\n");
+		for (int i=0; env[i] != NULL; i++)
+			fprintf(out, "%3d %s\n", i, env[i]);
+	} while (0);
 
-	fprintf(out, "#nr parameter\n");
-	for (int i=0; i<argc; i++)
-		fprintf(out, "%3d %s\n", i, argv[i]);
+	do {
+		fprintf(out, "#nr parameter\n");
+		for (int i=0; i<argc; i++)
+			fprintf(out, "%3d %s\n", i, argv[i]);
+	} while (0);
+
 }
