23e9500fbd4da36fb0b238415e3290f27828af75
Hans Ulrich Niedermann <hun@n-dimensional.de>
2016-06-20 03:39:43 +0200

    clean up rpm builds to work without fedpkg
    rpmbuild (and optionally mock) are sufficient.

--- m4/ndim-build-rpms.mk
+++ m4/ndim-build-rpms.mk
@@ -4,18 +4,19 @@ CLEAN_LOCAL_TARGETS += clean-local-ndim-build-rpms
 clean-local-ndim-build-rpms:
 	rm -rf rpm-build rpm-dist 'results_$(PACKAGE_TARNAME)'
 
-CLEANFILES += $(PACKAGE_TARNAME).spec
-EXTRA_DIST += $(PACKAGE_TARNAME).spec
+SPECFILE = $(PACKAGE_TARNAME).spec
+CLEANFILES += $(SPECFILE)
+EXTRA_DIST += $(SPECFILE)
 EXTRA_DIST += package.spec.in
-$(PACKAGE_TARNAME).spec: $(top_srcdir)/package.spec.in Makefile
+$(SPECFILE): $(top_srcdir)/package.spec.in Makefile
 	$(SED) \
 		-e 's,[@]distdir@,$(distdir),g' \
 		-e 's,[@]PACKAGE_NAME@,$(PACKAGE_NAME),g' \
 		-e 's,[@]PACKAGE_TARNAME@,$(PACKAGE_TARNAME),g' \
 		-e 's,[@]PACKAGE_URL@,$(PACKAGE_URL),g' \
-		-e "s,[@]RPM_VERSION@,$$(echo "$(PACKAGE_VERSION)" | $(SED) s/-.*//),g" \
-		-e "s,[@]RPM_RELEASE@,$$(echo "$(PACKAGE_VERSION)" | $(SED) s/.*-/git/),g" \
-		$(top_srcdir)/package.spec.in > $(PACKAGE_TARNAME).spec
+		-e "s,[@]RPM_VERSION@,$(RPM_VERSION),g" \
+		-e "s,[@]RPM_RELEASE@,$(RPM_RELEASE),g" \
+		$(top_srcdir)/package.spec.in > $(SPECFILE)
 
 if DO_RPMBUILD
 
@@ -30,27 +31,32 @@ rpm: dist-xz
 	mkdir  rpm-dist rpm-build
 	$(RPMBUILD) $(RPMBUILD_OPTS) -ta $(distdir).tar.xz
 
-endif
+if DO_MOCKBUILD
 
-if DO_FEDPKG
+PACKAGE_SRPM = $(PACKAGE_TARNAME)-$(RPM_VERSION)-$(RPM_RELEASE).src.rpm
 
-FEDPKG_OPTS =
-FEDPKG_OPTS += --module-name=$(PACKAGE_TARNAME)
-FEDPKG_OPTS += --dist=f$(NDIM_FEDORA_VERSION)
+SRPM_RPMBUILD_OPTS =
+SRPM_RPMBUILD_OPTS += --define 'dist %{nil}'
+SRPM_RPMBUILD_OPTS += --define "_sourcedir $${PWD}"
+SRPM_RPMBUILD_OPTS += --define "_srcrpmdir $${PWD}"
 
-mockbuild: dist-xz
-	$(FEDPKG) $(FEDPKG_OPTS) mockbuild
-if DO_EXTRA_i386_BUILD
-	$(FEDPKG) $(FEDPKG_OPTS) mockbuild --root $(NDIM_i386_MOCK_ROOT)
-endif
+srpm: $(PACKAGE_SRPM)
+$(PACKAGE_SRPM): dist-xz $(SPECFILE)
+	$(RPMBUILD) $(SRPM_RPMBUILD_OPTS) -bs $(SPECFILE)
 
-mockbuild-all: dist-xz
-	@echo SHELL="$$SHELL"; fail=""; succ=""; \
-	if test "x$(NDIM_RPM_ARCH)" = "xx86_64" && test "x$(NDIM_MAINPKG_NOARCH)" = "xno"; then \
+MOCK_OPTS =
+MOCK_OPTS += --resultdir=$(PWD)/results_$(PACKAGE_TARNAME)/$(RPM_VERSION)/$(RPM_RELEASE)
+
+mockbuild: $(PACKAGE_SRPM)
+	$(MOCK) $(MOCK_OPTS) --rebuild $(PACKAGE_SRPM)
+
+mockbuild-all: $(PACKAGE_SRPM)
+	@fail=""; succ=""; \
+	if test "x$(NDIM_RPM_ARCH)" = "xx86_64" && test "xnoarch" != "x$(${SED} -n '/^%package/q; s/^BuildArch:[[[:space:]]]*\(noarch\)[[[:space:]]]*/\1/p' "${srcdir}/package.spec.in")"; then \
 	for mockroot in /etc/mock/fedora-*-x86_64.cfg /etc/mock/epel-*-x86_64.cfg /etc/mock/fedora-*-i386.cfg /etc/mock/epel-*-i386.cfg; do \
 		if test -f "$$mockroot"; then :; else echo "No such mock config: $$mockroot"; exit 1; fi; \
-		echo "$(FEDPKG) $(FEDPKG_OPTS) mockbuild --root $$mockroot"; \
-		if $(FEDPKG) $(FEDPKG_OPTS) mockbuild --root "$$mockroot"; then \
+		echo "$(MOCK) $(MOCK_OPTS) --root $$mockroot --rebuild $(PACKAGE_SRPM)"; \
+		if $(MOCK) $(MOCK_OPTS) --root "$$mockroot" --rebuild $(PACKAGE_SRPM); then \
 			succ="$${succ} $$(basename "$${mockroot}")"; \
 		else \
 			fail="$${fail} $$(basename "$${mockroot}")"; \
@@ -59,8 +65,8 @@ mockbuild-all: dist-xz
 	else \
 	for mockroot in /etc/mock/{fedora,epel}-*-$(NDIM_RPM_ARCH).cfg; do \
 		if test -f "$$mockroot"; then :; else echo "No such mock config: $$mockroot"; exit 1; fi; \
-		echo "$(FEDPKG) $(FEDPKG_OPTS) mockbuild --root $$mockroot"; \
-		if $(FEDPKG) $(FEDPKG_OPTS) mockbuild --root "$$mockroot"; then \
+		echo "$(MOCK) $(MOCK_OPTS) --root $$mockroot --rebuild $(PACKAGE_SRPM)"; \
+		if $(MOCK) $(MOCK_OPTS) --root "$$mockroot" --rebuild $(PACKAGE_SRPM); then \
 			succ="$${succ} $$(basename "$${mockroot}")"; \
 		else \
 			fail="$${fail} $$(basename "$${mockroot}")"; \
@@ -80,5 +86,7 @@ mockbuild-all: dist-xz
 		echo "Some builds failed."; \
 		exit 2; \
 	fi
+
+endif
 
 endif
