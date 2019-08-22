56bd27767332cdd3f3bac6eb82619117a16cac86
Hans Ulrich Niedermann <hun@n-dimensional.de>
2019-08-22 03:52:13 +0200

    clearer binary-test

--- binary-test/binary-test.c
+++ binary-test/binary-test.c
@@ -8,27 +8,30 @@
 static
 void by_start_and_end(void)
 {
+  fputs("S&E: BEGIN\n", stdout);
   for (const char *p=data_start; p<data_end; ++p) {
     fputc(*p, stdout);
   }
-  fputs("S&E: done\n", stderr);
+  fputs("S&E: END\n", stdout);
 }
 
 
 static
 void by_start_and_size(void)
 {
+  fputs("S&S: BEGIN\n", stdout);
   const ssize_t written = fwrite(data_start, 1, data_size, stdout);
   if (written < 0) {
     perror("fwrite");
   } else {
     const size_t u_written = written;
     if (u_written < data_size) {
-      fputs("S&S: written less than everything\n", stderr);
+      fputs("S&S: written less than everything\n", stdout);
     } else {
-      fputs("S&S: written everything (or more)\n", stderr);
+      fputs("S&S: written everything (or more)\n", stdout);
     }
   }
+  fputs("S&S: END\n", stdout);
 }
 
 
