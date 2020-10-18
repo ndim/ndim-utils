380a4aa45ba43817c8b7eebb5555d4c2d7ef8bd7
Hans Ulrich Niedermann <hun@n-dimensional.de>
2020-10-18 19:43:13 +0200

    Stop exporting shell variables with special characters
    Environment variables containing special characters like `\r` can
    cause problems in programs they are exported to.
    
    Not exporting those variables means they are still available from
    the command line in interactive shells, as those shells source the
    files in `/etc/profile.d/*.sh`. However, all processes started by
    a command line shell will not see the variable.
    
    Case in point is Gnome failing to start on Fedora 33:
    
      * https://github.com/systemd/systemd/issues/17378
      * https://gitlab.gnome.org/GNOME/gnome-session/-/issues/70
      * https://gitlab.gnome.org/GNOME/gnome-session/-/issues/71

--- profile/ndim-utils-CRLF.sh
+++ profile/ndim-utils-CRLF.sh
@@ -1,11 +1,11 @@
-# define special characters for use in shell commands
+# Define special characters for use in shell commands, but do not
+# export them.  We only need them in interactive shells, and they
+# might confuse processes started by interactive shells.
+
 LF="
 "
-export LF
 
 CR=""
-export CR
 
 CRLF="
 "
-export CRLF
