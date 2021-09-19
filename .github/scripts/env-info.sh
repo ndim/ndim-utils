b0f90c8c621cc892006ba42e676f7660c8b9d8de
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-09-19 02:49:31 +0200

    gh action: find out how to make git describe work

--- .github/scripts/env-info.sh
+++ .github/scripts/env-info.sh
@@ -5,7 +5,21 @@ set -x
 pwd
 
 ls -d .git
+
 git status
+git branch -v
+git describe
+
+git fetch -v
+
+git status
+git branch -v
+git describe
+
+git fetch -v --tags
+
+git status
+git branch -v
 git describe
 
 :
