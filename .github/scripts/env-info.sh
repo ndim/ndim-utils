2b535f726f54f27b4a4edef55c1722e975c73445
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-09-19 04:36:40 +0200

    gh action: print build tool version information

--- .github/scripts/env-info.sh
+++ .github/scripts/env-info.sh
@@ -13,4 +13,16 @@ git tag --list -n10
 git describe
 git describe --always
 
+$SHELL --version
+
+automake --version
+autoconf --version
+libtoolize --version
+gettextize --version
+pkg-config --version
+
+gcc --version
+clang --version
+make --version
+
 :
